4afb217ecf8c8ae0af2e7945859f963e85331ad3
pengliang <624151713@qq.com>
2020-09-13 15:28:53 +0800

    update

--- deploy.js
+++ deploy.js
@@ -1,24 +1,26 @@
 const { deploy } = require('sftp-sync-deploy')
 const { HOST: host, PASSWORD: password, REMOTEDIR: remoteDir } = process.env
 
-let config = {
-  host, // Required.
-  username: 'root', // Required.
-  password, // Optional.
-  localDir: './public', // Required, Absolute or relative to cwd.
-  remoteDir, // Required, Absolute path only.
-}
+;(async () => {
+  let config = {
+    host, // Required.
+    username: 'root', // Required.
+    password, // Optional.
+    localDir: './public', // Required, Absolute or relative to cwd.
+    remoteDir, // Required, Absolute path only.
+  }
 
-let options = {
-  dryRun: false, // Enable dry-run mode. Default to false
-  exclude: [
-    // exclude patterns (glob)
-    'node_modules',
-    'src/**/*.spec.ts',
-  ],
-  excludeMode: 'remove', // Behavior for excluded files ('remove' or 'ignore'), Default to 'remove'.
-  forceUpload: false, // Force uploading all files, Default to false(upload only newer files).
-  concurrency: 100, // Max number of SFTP tasks processed concurrently. Default to 100.
-}
+  let options = {
+    dryRun: false, // Enable dry-run mode. Default to false
+    exclude: [
+      // exclude patterns (glob)
+      'node_modules',
+      'src/**/*.spec.ts',
+    ],
+    excludeMode: 'remove', // Behavior for excluded files ('remove' or 'ignore'), Default to 'remove'.
+    forceUpload: false, // Force uploading all files, Default to false(upload only newer files).
+    concurrency: 100, // Max number of SFTP tasks processed concurrently. Default to 100.
+  }
 
-return deploy(config, options)
+  return deploy(config, options)
+})()
